540eb397f5cb05d06f679f5dfa7a1b4badc8f45f
Felipe Noobrega <felipenobregacp@gmail.com>
2024-08-30 16:56:17 -0300

    feat(home):layout ajustado com o basico de ux

--- src/App.js
+++ src/App.js
@@ -2,77 +2,149 @@ import React, { useState } from 'react';
 import forge from 'node-forge';
 import './App.css';
 
+const styles = {
+  App: {
+    margin:'auto',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+    padding: '20px',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    maxWidth: '600px',
+    width: '100%',
+  },
+
+  AppHeader: {
+    textAlign: 'center',
+  },
+
+  h1: {
+    
+    fontSize: '40px',
+    marginBottom: '20px',
+    color:'#fff'
+  },
+
+  textarea: {
+    width: '100%',
+    padding: '10px',
+    marginTop: '10px',
+    marginBottom: '20px',
+    borderRadius: '4px',
+    border: '1px solid #cccccc',
+    fontSize: '16px',
+  },
+
+  button: {
+    backgroundColor: '#007bff',
+    color: 'white',
+    padding: '10px 15px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    marginRight: '10px',
+    transition: 'background-color 0.3s ease',
+  },
+
+  h2: {
+    color: '#fff',
+    fontSize: '20px',
+    marginTop: '30px',
+    marginBottom: '10px',
+  },
+
+  ul: {
+    listStyleType: 'none',
+    padding: 0,
+  },
+
+  li: {
+    backgroundColor: '#f9f9f9',
+    padding: '10px',
+    marginBottom: '10px',
+    borderRadius: '4px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  liButton: {
+    backgroundColor: '#28a745',
+    marginRight: 0,
+    borderRadius:'8px'
+  },
+};
+
 function App() {
-  // Estados para armazenar o texto plano, documentos, chave privada e chave pública
   const [plainText, setPlainText] = useState('');
   const [documents, setDocuments] = useState([]);
   const [privateKey, setPrivateKey] = useState(null);
   const [publicKey, setPublicKey] = useState(null);
+  const [signature, setSignature] = useState(null);
+  const [hasGeneratedKeys, setHasGeneratedKeys] = useState(false);
 
-  // Função para gerar chaves RSA de 2048 bits e armazená-las nos estados
   const generateRSAKeys = () => {
-    const keypair = forge.pki.rsa.generateKeyPair({ bits: 2048 }); // Geração do par de chaves
-    const privateKeyPEM = forge.pki.privateKeyToPem(keypair.privateKey); // Converter chave privada para PEM
-    const publicKeyPEM = forge.pki.publicKeyToPem(keypair.publicKey);   // Converter chave pública para PEM
-    
-    setPrivateKey(privateKeyPEM); // Armazenar a chave privada no estado
-    setPublicKey(publicKeyPEM);   // Armazenar a chave pública no estado
-    
-    // Exibir as chaves no console (opcional)
+    const keypair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
+    const privateKeyPEM = forge.pki.privateKeyToPem(keypair.privateKey);
+    const publicKeyPEM = forge.pki.publicKeyToPem(keypair.publicKey);
+
+    setPrivateKey(privateKeyPEM);
+    setPublicKey(publicKeyPEM);
+    setHasGeneratedKeys(true);
+
     console.log("Chave Privada RSA 2048:");
     console.log(privateKeyPEM);
     console.log("\nChave Pública RSA 2048:");
     console.log(publicKeyPEM);
   };
 
-  // Função para assinar o documento utilizando a chave privada
   const signDocument = () => {
-    if (!privateKey) { // Verifica se a chave privada foi gerada
+    if (!privateKey) {
       alert('Gere as chaves primeiro!');
       return;
     }
 
-    // Criação do hash SHA-256 do texto plano
     const md = forge.md.sha256.create();
     md.update(plainText, 'utf8');
 
-    // Assinatura do hash com a chave privada
     const privateKeyObj = forge.pki.privateKeyFromPem(privateKey);
     const signature = privateKeyObj.sign(md);
 
-    // Adicionar o documento assinado à lista de documentos
+    setSignature(signature); // Armazena a assinatura no estado
+    alert('Documento assinado!');
+  };
+
+  const saveDocument = () => {
+    if (!signature) {
+      alert('O documento precisa ser assinado antes de ser salvo.');
+      return;
+    }
+
     setDocuments([...documents, { plainText, signature }]);
-    setPlainText(''); // Limpar o campo de texto após a assinatura
+    setPlainText('');
+    setSignature(null); // Reseta a assinatura após salvar
   };
 
-  // Função para salvar o documento sem assiná-lo
-  const saveWithoutSigning = () => {
-    // Adicionar o documento sem assinatura à lista de documentos
-    setDocuments([...documents, { plainText, signature: null }]);
-    setPlainText(''); // Limpar o campo de texto após salvar
-  };
-
-  // Função para verificar a assinatura de um documento
   const verifySignature = (signature, originalMessage) => {
-    if (!signature) { // Verifica se o documento foi assinado
+    if (!signature) {
       alert('Este documento não foi assinado.');
       return;
     }
 
-    if (!publicKey) { // Verifica se a chave pública foi gerada
+    if (!publicKey) {
       alert('Gere as chaves primeiro!');
       return;
     }
 
-    // Criação do hash SHA-256 do texto original
     const md = forge.md.sha256.create();
     md.update(originalMessage, 'utf8');
 
-    // Verificação da assinatura usando a chave pública
     const publicKeyObj = forge.pki.publicKeyFromPem(publicKey);
     const verified = publicKeyObj.verify(md.digest().bytes(), signature);
 
-    // Exibir mensagem de resultado da verificação
     if (verified) {
       alert('Assinatura válida!');
     } else {
@@ -81,15 +153,16 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Assinador Digital com RSA</h1>
+    <div style={styles.App}>
+      <header style={styles.AppHeader}>
+        <h1 style={styles.h1}>Assinador Digital com RSA</h1>
         
         {/* Botão para gerar as chaves RSA */}
-        <button onClick={generateRSAKeys}>Gerar Chaves RSA</button>
+        <button style={styles.button} onClick={generateRSAKeys}>Gerar Chaves RSA</button>
         
         {/* Campo de texto para o usuário digitar o documento */}
         <textarea
+          style={styles.textarea}
           value={plainText}
           onChange={(e) => setPlainText(e.target.value)}
           rows="4"
@@ -97,20 +170,22 @@ function App() {
           placeholder="Digite seu texto aqui"
         />
         <br />
-        {/* Botão para assinar o documento */}
-        <button onClick={signDocument}>Assinar Documento</button>
-        
-        {/* Botão para salvar o documento sem assinar */}
-        <button onClick={saveWithoutSigning}>Salvar sem Assinar</button>
 
-        <h2>Documentos</h2>
-        {/* Lista de documentos assinados ou salvos */}
-        <ul>
+        {/* Botões só aparecem se as chaves foram geradas */}
+        {hasGeneratedKeys && (
+          <>
+            <button style={styles.button} onClick={signDocument}>Assinar Documento</button>
+            <button style={styles.button} onClick={saveDocument}>Salvar</button>
+          </>
+        )}
+
+        <h2 style={styles.h2}>Documentos</h2>
+        
+        <ul style={styles.ul}>
           {documents.map((doc, index) => (
-            <li key={index}>
+            <li key={index} style={styles.li}>
               {doc.plainText}
-              {/* Botão para verificar a assinatura do documento */}
-              <button onClick={() => verifySignature(doc.signature, doc.plainText)}>Verificar Assinatura</button>
+              <button style={styles.liButton} onClick={() => verifySignature(doc.signature, doc.plainText)}>Verificar Assinatura</button>
             </li>
           ))}
         </ul>
@@ -120,3 +195,5 @@ function App() {
 }
 
 export default App;
+
+
